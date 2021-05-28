bfebcd8a655338353baf452f4fcaa4bc175360e5
AlekseiAlekhin <alekhin.exceedteam@gmail.com>
2021-05-28 18:10:33 +0300

    some style fix

--- src/components/pageAuthorization/FormAuthorization.js
+++ src/components/pageAuthorization/FormAuthorization.js
@@ -4,15 +4,29 @@ import React, {memo} from 'react';
 import {useDispatch} from "react-redux";
 import {authorizationUser} from "../../actions /actionsCreator";
 import {useRef} from "react";
+import {Collapse, Form, Navbar,Nav} from "bootstrap-4-react/lib/components";
+import {BDiv} from "bootstrap-4-react/lib/components/dom";
 
 export const FormAuthorization = memo(()=>{
     let dispatch = useDispatch();
     let name = useRef();
     let pass = useRef();
-    return <div>
-        <input ref={name} type={"input"} name={'name'} required/>
-        <input ref={pass} type="password" name={'password'} required/>
-        <Button type={'submit'} dark onClick={()=>dispatch(authorizationUser({userName:name.current.value, password:pass.current.value}))}>Next</Button>
-        <Link to={'/registration'}>Registration</Link>
+    return <div style={{padding: '15px 20px', boxSizing: 'border-box'}}>
+        <Navbar expand="lg" dark style={{ backgroundColor: '#000' }} mb="3">
+            <Navbar.Brand href="#">Your ToDo List</Navbar.Brand>
+            <Navbar.Toggler target="#navbarColor1" />
+            <Collapse navbar id="navbarColor3">
+                <Navbar.Nav mr="auto">
+                </Navbar.Nav>
+                <Button type={'button'} dark disabled>logIn</Button>
+                <Link to={'/registration'}><Button type={'button'} dark>Registration</Button></Link>
+            </Collapse>
+        </Navbar>
+        <BDiv mx="auto" shadow="lg" mb="5" bg="light" rounded style={{maxWidth: '400px',padding: '15px 20px', boxSizing: 'border-box'}}>
+            <h2>LogIn</h2>
+            <BDiv style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}><p>Login</p><input ref={name} type={"input"} name={'name'} required style={{marginBottom:'5px'}}/></BDiv>
+            <BDiv style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}><p>Password</p><input ref={pass} type="password" name={'password'} required style={{marginBottom:'5px'}}/></BDiv>
+            <Button type={'submit'} dark onClick={()=>dispatch(authorizationUser({userName:name.current.value, password:pass.current.value}))}>Next</Button>
+        </BDiv>
     </div>
 })
--- src/components/pageRegistration/Registration.js
+++ src/components/pageRegistration/Registration.js
@@ -1,7 +1,9 @@
 import {Button} from 'bootstrap-4-react'
-import {memo, useRef} from "react"
-import {useHistory} from "react-router-dom";
+import React, {memo, useRef} from "react"
+import {Link, useHistory} from "react-router-dom";
 import {createUserRegistration} from "../Services/todoService";
+import {Collapse, Navbar} from "bootstrap-4-react/lib/components";
+import {BDiv} from "bootstrap-4-react/lib/components/dom";
 
 
 export const Registration = memo(() => {
@@ -14,9 +16,21 @@ export const Registration = memo(() => {
     }
 
     return <div>
-        <h1>REGISTRATION</h1>
-        <input ref={name} name={'name'}/>
-        <input ref={pass} name={'password'} />
-        <Button dark onClick={registrUser}>Done</Button>
+        <Navbar expand="lg" dark style={{ backgroundColor: '#000' }} mb="3">
+            <Navbar.Brand href="#">Your ToDo List</Navbar.Brand>
+            <Navbar.Toggler target="#navbarColor1" />
+            <Collapse navbar id="navbarColor3">
+                <Navbar.Nav mr="auto">
+                </Navbar.Nav>
+                <Link to={'/login'}><Button type={'button'} dark>logIn</Button></Link>
+                <Button type={'button'} dark disabled>Registration</Button>
+            </Collapse>
+        </Navbar>
+        <BDiv mx="auto" shadow="lg" mb="5" bg="light" rounded style={{maxWidth: '400px',padding: '15px 20px', boxSizing: 'border-box'}}>
+            <h2>REGISTRATION</h2>
+            <BDiv style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}><p>Login</p><input ref={name} name={'name'} style={{marginBottom:'5px'}}/></BDiv>
+            <BDiv style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}><p>Password</p><input ref={pass} name={'password'} style={{marginBottom:'5px'}}/></BDiv>
+            <Button dark onClick={registrUser}>Done</Button>
+        </BDiv>
     </div>
 })
